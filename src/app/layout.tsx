a81b3cdbdacd1d78a4f976515d5c16bb46a37600
inu-wanwan <rixy.takagi@gmail.com>
2024-11-28 14:41:05 +0900

    Add analysis tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google"
 import { ThemeProvider } from "../components/theme-provider"
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
+import { Metadata } from 'next'
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,6 +20,49 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" suppressHydrationWarning>
+      <head>
+        {/* Google Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-M2C7ZQ1Y3D"
+          strategy="afterInteractive" // ページがインタラクティブになった後に読み込む
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M2C7ZQ1Y3D');
+          `}
+        </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s) {
+              if(f.fbq)return;
+              n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1533897907329752');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1533897907329752&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
